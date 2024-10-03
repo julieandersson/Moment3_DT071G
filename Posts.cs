d6862d6c1e4ea8785f871effd75024cb1341a00c
julieandersson <juan2301@student.miun.se>
2024-10-03 12:34:10 +0200

    Skapar grundestrukturen med huvudmenyn och post-klass i filen Posts.cs

---
+++ Posts.cs
@@ -0,0 +1,8 @@
+namespace GuestbookApp
+{
+    public class Post
+    {
+        public string? Author { get; set; }
+        public string? Message { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+using System.Globalization;
+
+namespace GuestbookApp
+{
+    class Program
+    {
+        // Skapar lista för att lagra alla inlägg i gästboken
+        private static List<Post> guestbookPosts = new List<Post>();
+
+        static void Main(string[] args)
+        {
+            MainMenu(); // Visa huvudmenyn
+        }
+
+        public static void MainMenu()
+        {
+
+        }
+
+        public static void AddPost() { }
+        public static void DeletePost() { }
+        public static void DisplayPosts() { }
+        public static void SavePosts() { }
+        public static void LoadPosts() { }
+        public static void ExitProgram() { }
+    }
+}
